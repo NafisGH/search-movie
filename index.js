2ba611343f227ce02143e150c49da4f279adb469
Nafis Garifullin <garifullinn@gmail.com>
2024-03-03 15:47:25 +0500

    search movie

---
+++ index.js
@@ -0,0 +1,147 @@
+const inputSerch = document.querySelector(".js-input-serch");
+const btnSearch = document.querySelector(".js-btn-search");
+const moviesList = document.querySelector(".js-movies-list");
+
+btnSearch.addEventListener("click", getMovie);
+
+function getMovie() {
+  const nameMovie = inputSerch.value;
+
+  fetch(`https://www.omdbapi.com/?s=${nameMovie}&apikey=f7c7b9db`)
+    .then((data) => data.json())
+    .then((movie) => {
+      const movies = movie.Search;
+      console.log(movie.Search);
+
+      moviesList.innerHTML = "";
+
+      movies.forEach((movie) => {
+        const movieElement = document.createElement("div");
+        movieElement.classList.add("movie-elem");
+        // Сохраняем imdbID фильма в атрибуте data-imdbid
+        movieElement.dataset.imdbid = movie.imdbID;
+
+        const wrapperMovieDiscription = document.createElement("div");
+        wrapperMovieDiscription.classList.add("wrapper-movi-discription");
+
+        const imgElem = document.createElement("img");
+        imgElem.classList.add("img-movie");
+        imgElem.src = movie.Poster;
+        movieElement.appendChild(imgElem);
+
+        const titleElem = document.createElement("h2");
+        titleElem.classList.add("title-movie");
+        titleElem.textContent = movie.Title;
+        wrapperMovieDiscription.appendChild(titleElem);
+
+        const yearElem = document.createElement("p");
+        yearElem.classList.add("year-movie");
+        yearElem.textContent = movie.Year;
+        wrapperMovieDiscription.appendChild(yearElem);
+
+        const typeElem = document.createElement("p");
+        typeElem.classList.add("type-elem");
+        typeElem.textContent = movie.Type;
+        wrapperMovieDiscription.appendChild(typeElem);
+
+        const idElem = document.createElement("p");
+        idElem.textContent = movie.imdbID;
+
+        movieElement.appendChild(wrapperMovieDiscription);
+        moviesList.appendChild(movieElement);
+      });
+    })
+    .catch((error) => {
+      console.error("Ошибка при загрузке фильма", error);
+    });
+  afterSearch();
+  inputSerch.value = "";
+}
+
+const moviesListNode = document.querySelector(".js-movies-list");
+
+moviesListNode.addEventListener("click", handelClickMovie);
+
+function handelClickMovie(event) {
+  const clickedMovie = event.target.closest(".movie-elem");
+  console.log(clickedMovie);
+  if (clickedMovie) {
+    const imdbId = clickedMovie.dataset.imdbid;
+    fetch(`https://www.omdbapi.com/?i=${imdbId}&apikey=f7c7b9db`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let clickedMovieData = data;
+        console.log(clickedMovieData);
+        renderMivieImdbId(clickedMovieData);
+      });
+  }
+}
+
+function renderMivieImdbId(clickedMovieData) {
+  console.log(clickedMovieData);
+  moviesList.innerHTML = "";
+  const wrapperDescriptionFilm = document.createElement("div");
+
+  const actors = document.createElement("p");
+  actors.classList.add("actors");
+  actors.textContent = `Actors: ${clickedMovieData.Actors}`;
+  wrapperDescriptionFilm.append(actors);
+
+  const awards = document.createElement("p");
+  awards.classList.add("awards");
+  awards.textContent = `Awards: ${clickedMovieData.Awards}`;
+  wrapperDescriptionFilm.append(awards);
+
+  const boxOffice = document.createElement("p");
+  boxOffice.classList.add("boxOffice");
+  boxOffice.textContent = `BoxOffice: ${clickedMovieData.BoxOffice}`;
+  wrapperDescriptionFilm.append(boxOffice);
+
+  const country = document.createElement("p");
+  country.classList.add("country");
+  country.textContent = `Country: ${clickedMovieData.Country}`;
+  wrapperDescriptionFilm.append(country);
+
+  const dvd = document.createElement("p");
+  dvd.classList.add("dvd");
+  dvd.textContent = `DVD: ${clickedMovieData.DVD}`;
+  wrapperDescriptionFilm.append(dvd);
+
+  const director = document.createElement("p");
+  director.classList.add("director");
+  director.textContent = `Director: ${clickedMovieData.Director}`;
+  wrapperDescriptionFilm.append(director);
+
+  const genre = document.createElement("p");
+  genre.classList.add("genre");
+  genre.textContent = `Genre: ${clickedMovieData.Genre}`;
+  wrapperDescriptionFilm.append(genre);
+
+  const language = document.createElement("p");
+  language.classList.add("language");
+  language.textContent = `Language: ${clickedMovieData.Language}`;
+  wrapperDescriptionFilm.append(language);
+
+  const metascore = document.createElement("p");
+  metascore.classList.add("metascore");
+  metascore.textContent = `Metascore: ${clickedMovieData.Metascore}`;
+  wrapperDescriptionFilm.append(metascore);
+
+  const plot = document.createElement("p");
+  plot.classList.add("plot");
+  plot.textContent = `Plot: ${clickedMovieData.Plot}`;
+  wrapperDescriptionFilm.append(plot);
+
+  const poster = document.createElement("img");
+  poster.classList.add("poster");
+  poster.src = `${clickedMovieData.Poster}`;
+  wrapperDescriptionFilm.append(poster);
+
+  moviesList.append(wrapperDescriptionFilm);
+}
+
+function afterSearch() {
+  btnSearch.classList.add("btn-search_black");
+}
